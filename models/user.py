a8f8e56e810e3575933f1d98896de5c95bc348ba
Matej Ramuta <matej.ramuta@gmail.com>
2019-10-22 23:25:03 +0200

    emails not needed because it will be a username/password-only app (no email messages)

--- models/user.py
+++ models/user.py
@@ -11,12 +11,15 @@ class Session(ndb.Model):
 class User(ndb.Model):
     first_name = ndb.StringProperty()
     last_name = ndb.StringProperty()
-    email_address = ndb.StringProperty(repeated=True)  # user can have multiple email addresses
+
+    username = ndb.StringProperty()
+    password_hash = ndb.StringProperty()  # use bcrypt: http://zetcode.com/python/bcrypt/, rounds=12
+
     admin = ndb.BooleanProperty(default=False)
     suspended = ndb.BooleanProperty(default=False)
     session_tokens = ndb.StructuredProperty(Session, repeated=True)
 
     # standard model fields
-    created = ndb.DateTimeProperty(auto_now_add=True)
+    created = ndb.DateTimeProperty(auto_now_add=True)  # use https://github.com/miguelgrinberg/Flask-Moment
     updated = ndb.DateTimeProperty(auto_now=True)
     deleted = ndb.BooleanProperty(default=False)
